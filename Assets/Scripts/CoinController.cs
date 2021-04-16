c4ff7704ed536a588c6a40f411f10391d4ff5c6d
Bruno Lončar <bruno.loncar@outlook.com>
2021-04-16 21:28:29 +0200

    Homework

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CollectableType
+{
+    Silver, 
+    Gold
+}
+
+public class CoinController : MonoBehaviour
+{
+    [SerializeField]
+    private CollectableType type = CollectableType.Gold;
+
+    [SerializeField]
+    private float rotationSpeed = 60;
+
+    private void Start()
+    {
+        GetComponent<Renderer>().material.color = GetColor();
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.World);
+    }
+
+    public int GetPoints()
+    {
+        switch (type)
+        {
+            case CollectableType.Silver:
+                return 1;
+            case CollectableType.Gold:
+                return 5;
+        }
+        return 0;
+    }
+
+    public Color GetColor()
+    {
+        switch (type)
+        {
+            case CollectableType.Silver:
+                return Color.gray;
+            case CollectableType.Gold:
+                return Color.yellow;
+        }
+        return Color.grey;
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public interface ICollectionHandler
+{
+    void PlayerDidCollectItem(GameObject item);
+}
+
+public class GameManager : MonoBehaviour, ICollectionHandler
+{
+    private int numberOfCollectables;
+    private int numberOfItemsCollected = 0;
+
+    private int points = 0;
+
+    public PlayerController Player;
+    public UIManager uiManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player.collectionHandler = this;
+        numberOfCollectables = GameObject.FindGameObjectsWithTag(Tags.Collectable).Length;
+
+        uiManager.UpdatePoints(points);
+    }
+
+    public void PlayerDidCollectItem(GameObject item)
+    {
+        points += item.GetComponent<CoinController>().GetPoints();
+        uiManager.UpdatePoints(points);
+        Destroy(item);
+
+        numberOfItemsCollected++;
+
+        if (numberOfItemsCollected >= numberOfCollectables)
+        {
+            SceneManager.LoadScene("Level2");
+        }
+    }
+}
